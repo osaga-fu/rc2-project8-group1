7985eb8b5828ee9e59124682c30756bd062b1df9
Emii <151835514+Emilly3008@users.noreply.github.com>
2024-02-14 09:23:40 +0100

    añadimos buscador

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -1,8 +1,37 @@
-
-import "./List.css"
+import { useState } from "react";
+import "./List.css";
 export const List = () => {
-    const 
+  const [busqueda, setBusqueda] = useState("");
+  const [resultados, setResultados] = useState([]);
+  const handleBusqueda = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/busqueda?query=${busqueda}`
+      );
+      const data = await response.json();
+      setResultados(data);
+    } catch (error) {
+      console.error("Error al buscar", error);
+    }
+  };
   return (
-    <div className="listContainer">List</div>
-  )
-}
+    <div className="listContainer">
+      <button className="searchButton" onClick={handleBusqueda}>
+        <img className= "searchIcon" src="../../../src/imgs/Search.svg" alt="icono buscador" />
+      </button>
+
+      <input
+        className="searchInput"
+        type="text"
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+        placeholder="Buscar libro..."
+      />
+      <ul>
+        {resultados.map((resultado) => (
+          <li key={resultado.id}>{resultado.titulo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
