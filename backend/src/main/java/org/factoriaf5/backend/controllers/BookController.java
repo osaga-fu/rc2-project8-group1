b07bac3e18868d1623b1f4bcd49bcd5cabe4161d
mfg1985 <mfg1985@me.com>
2024-02-14 12:17:51 +0100

    create post method

--- backend/src/main/java/org/factoriaf5/backend/controllers/BookController.java
+++ backend/src/main/java/org/factoriaf5/backend/controllers/BookController.java
@@ -7,23 +7,33 @@ import org.factoriaf5.backend.persistence.BooksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
 public class BookController {
-    
-    
+
     private BooksRepository repository;
 
-    public BookController(@Autowired BooksRepository repository){
+    public BookController(@Autowired BooksRepository repository) {
         this.repository = repository;
     }
 
     @GetMapping("/books")
     public List<Book> searchBooks(@RequestParam("query") String query) {
         return repository.searchBooks("%" + query + "%");
-    } 
+    }
+
+    @PostMapping("/books")
+    public BookResponse createBook(@RequestBody BookRequest request) {
+        Book book = new Book(request.getBook_id(), request.getTitle(), request.getAuthor(), request.getIsbn(),
+                request.getSectionCode());
+        Book savedBook = repository.save(book);
+        return new BookResponse(savedBook.getBook_id(), savedBook.getTitle(), savedBook.getAuthor(),
+                savedBook.getIsbn(), savedBook.getSectionCode());
+    }
 
 }
