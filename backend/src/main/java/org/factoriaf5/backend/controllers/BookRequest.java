dbfd3df43912f1b9eb7bb6bb0b656f6838f32f68
Johnny <jonathanbc2012@gmail.com>
2024-02-14 10:00:51 +0100

    book request, response, repository classes created

---
+++ backend/src/main/java/org/factoriaf5/backend/controllers/BookRequest.java
@@ -0,0 +1,57 @@
+package org.factoriaf5.backend.controllers;
+
+public class BookRequest {
+    private Long book_id;
+    private String title;
+    private String author;
+    private String isbn;
+    private String section;
+
+    public BookRequest(Long book_id, String title, String author, String isbn, String section) {
+        this.book_id = book_id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.section = section;
+    }
+
+    public Long getBook_id() {
+        return book_id;
+    }
+
+    public void setBook_id(Long book_id) {
+        this.book_id = book_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+}
---
+++ backend/src/main/java/org/factoriaf5/backend/persistence/BooksRepository.java
@@ -0,0 +1,8 @@
+package org.factoriaf5.backend.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Long> {
+}
