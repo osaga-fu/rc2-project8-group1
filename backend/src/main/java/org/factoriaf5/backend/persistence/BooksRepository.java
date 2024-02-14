498ef5bb446a08524b08123a0b7caea2c01f63d7
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-02-14 10:43:23 +0100

    controller updated and properties modified

--- backend/src/main/java/org/factoriaf5/backend/persistence/BooksRepository.java
+++ backend/src/main/java/org/factoriaf5/backend/persistence/BooksRepository.java
@@ -1,8 +1,17 @@
 package org.factoriaf5.backend.persistence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BooksRepository extends JpaRepository<Book, Long> {
+
+    @Query("SELECT b FROM Book b WHERE lower (b.title) LIKE lower (:query)" +
+        "OR lower(b.author) LIKE lower(:query)" +
+        "OR lower(b.isbn) LIKE lower(:query)" +
+        "OR lower(b.section_code) LIKE lower(:query)" )
+        List<Book> searchBooks(String query);
 }
